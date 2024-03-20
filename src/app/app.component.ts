7aecce656c3c3db835e2c93b610d7e4d4085280b
lyam1100 <164102030+lyam1100@users.noreply.github.com>
2024-03-20 15:26:01 -0600

    ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularBootCamp';
+  isActive = false; 
+
+  public ChangeStatus(){
+    this.isActive = !this.isActive;
+  }
 }
